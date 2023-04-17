9285cf82708f9647f349db3de7fe6f2d992feadc
rjalvarez27 <raul509@gmail.com>
2023-04-17 21:39:42 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -7,26 +7,39 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'User'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    email = Column(String(50), nullable=False, unique=True)
+    username = Column(String(50), nullable=False, unique=True)
+    password = Column(String(50), nullable=False)
 
-    def to_dict(self):
-        return {}
+
+class Persons(Base):
+    __tablename__ = 'Persons'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40), nullable=False)
+    birth_Year = Column(String(20))
+    skin_color = Column(String(80))
+    height = Column(String(8))
+    eye_color = Column(String(8))
+
+
+class Planets (Base):
+    __tablename__ = 'Planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), nullable=False)
+    climate = Column(String(20))
+    population = Column(Integer(10))
+    orbital_period = Column(String(8))
+    rotate_period = Column(String(8))
+
+class Favorites (Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey=True)
+    user = relationship(User)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
