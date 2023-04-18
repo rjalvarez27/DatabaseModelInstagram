9198ec748978135d3136fed808a4bf32ca054403
rjalvarez27 <raul509@gmail.com>
2023-04-18 22:59:58 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -13,45 +13,52 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String(50), nullable=False, unique=True)
     username = Column(String(50), nullable=False, unique=True)
+    firstname = Column(String(10), nullable=True)
+    lastname = Column(String(10), nullable= True)
     password = Column(String(50), nullable=False)
-    favorites =  relationship('favorite')
+    follower =  relationship('follower')
+    post = relationship('post')
+    comment = relationship('comment')
 
-class Persons(Base):
-    __tablename__ = 'Persons'
+class Media(Base):
+    __tablename__ = 'Media'
     id = Column(Integer, primary_key=True)
-    name = Column(String(40), nullable=False)
-    birth_Year = Column(String(20))
-    skin_color = Column(String(80))
-    height = Column(String(8))
-    eye_color = Column(String(8))
-    favorites =  relationship('favorite')
-
-class Planets(Base):
-    __tablename__ = 'Planets'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(20), nullable=False)
-    climate = Column(String(20))
-    population = Column(Integer)
-    orbital_period = Column(String(8))
-    rotate_period = Column(String(8))
-    favorites =  relationship('favorite')
+    type = Column(Integer, nullable = False)
+    url = Column(String(80),nullable= False) 
+    Post_id = Column(Integer, ForeignKey('Post.id'), nullable= False)
     
-class Favorite(Base):
-    __tablename__ = "favorite"    
-    id = Column(Integer, primary_key=True)
-    #Personaje
-    persons = relationship(Persons)
-    persons_id = Column(Integer, ForeignKey('Persons.id'), nullable=True)
-    
-    #Planeta
-    planets = relationship(Planets)
-    planets_id = Column(Integer, ForeignKey('Planets.id'), nullable = True)
 
-    #Usuario
+
+class Post(Base):
+    __tablename__ = 'Post'
+    id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('User.id'), nullable= False)
-    user = relationship(User)
-
+    user = relationship(User)    
+    comment = relationship('Comment')
+    media = relationship('Media')
     
+    
+    
+class Comment(Base):
+    __tablename__ = 'Comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(120), nullable=True)   
+    author_id = Column(Integer, ForeignKey('User.id'), nullable= False)
+    user = relationship(User) 
+    post_id =  Column(Integer, ForeignKey('Post.id'), nullable= False)
+    
+    
+
+class Follower(Base):
+    __tablename__ = "Follower"    
+    id = Column(Integer, primary_key=True)     
+    #User fron id
+    user_From_id = Column(Integer, ForeignKey('User.id'), nullable= True)
+    user = relationship(User)      
+    #user to id 
+    user_to_id = Column(Integer, ForeignKey('User.id'), nullable= True)
+    user = relationship(User)   
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
